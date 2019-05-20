1c552a5df2389a373e2bec1bee9232803f1c01ed
RonQian <qianyr@dade>
2019-05-20 14:45:30 +0800

    updata build.gradle

--- app/src/main/java/com/qry/ronqian/MainActivity.java
+++ app/src/main/java/com/qry/ronqian/MainActivity.java
@@ -1,13 +1,39 @@
 package com.qry.ronqian;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.qry.base.presenter.IBasePresenter;
+import com.qry.base.view.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.mTv_hello)
+    TextView mTvHello;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+        mTvHello.setText("我是MVP的框架");
+    }
+
+    @Override
+    protected IBasePresenter createPresenter() {
+        return null;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
